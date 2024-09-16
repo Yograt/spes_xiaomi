7cbe096e577a1c31c5969de3f939e65ce1fd5126
spesmynuts <rianemaster1221@gmail.com>
2024-09-16 18:11:03 +0800

    spes: @ProjectBlaze BRINGUP

--- blaze_spes.mk
+++ blaze_spes.mk
@@ -14,11 +14,11 @@ $(call inherit-product, device/xiaomi/spes/device.mk)
 # MiuiCamera
 $(call inherit-product-if-exists, vendor/xiaomi/miuicamera/config.mk)
 
-# Inherit some common risingOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common ProjectBlaze stuff.
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
 
 # Product Specifics
-PRODUCT_NAME := lineage_spes
+PRODUCT_NAME := blaze_spes
 PRODUCT_DEVICE := spes
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11
@@ -29,23 +29,10 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 # Boot animaton
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# RisingOS Flags
-WITH_GMS := true
-PRODUCT_NO_CAMERA := true
-# TARGET_CORE_GMS := true
-TARGET_ENABLE_BLUR := false
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_HAS_UDFPS := false
-TARGET_USE_GOOGLE_TELEPHONY := true
-RISING_MAINTAINER := spesmynuts
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    RISING_CHIPSET="Snapdragon 680" \
-    RISING_MAINTAINER="spesmynuts"
-
-# Launchers
-TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := false
-TARGET_DEFAULT_PIXEL_LAUNCHER := false
+# Blaze Official Stuff
+BLAZE_BUILD_TYPE := UOFFICIAL
+BLAZE_MAINTAINER := spesmynuts
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
@@ -55,7 +42,3 @@ BUILD_FINGERPRINT := "Redmi/spes/spes:13/TKQ1.221114.001/V14.0.5.0.TGCINXM:user/
 
 PRODUCT_SYSTEM_NAME := spes_global
 PRODUCT_SYSTEM_DEVICE := spes
-
-# Gapps (Basic)
-WITH_GAPPS := true
-TARGET_BUILD_GOOGLE_TELEPHONY := true
